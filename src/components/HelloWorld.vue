5efb6cc4253b7bf6522d773a99005d8397457017
Ludovic QUENEHEN <ludovic.quenehen@placement-direct.fr>
2023-04-16 19:10:58 +0200

    project setup vue3 +  typescript +  tailwindcss

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  msg: { type: String, required: true }
+})
+</script>
+
+<template>
+  <h1 class="text-center text-red-500 underline">{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      light: '#FFFFFF',
+      dark: '#1C1F27',
+      gray: '#5F6C85',
+      neutral: '#E2E8F0',
+      // status background
+      'highly-active-light': '#6938EF',
+      'active-light': '#ECFDF3',
+      'overbooked-light': '#FEF3F2',
+      'observer-light': '#EEF4FF',
+      'sleeper-light': '#FFFAEB',
+      'invited-light': '#F8F9FC',
+      // status font
+      'highly-active': '#6938EF',
+      active: '#03874C',
+      overbooked: '#D92D20',
+      observer: '#444CE7',
+      sleeper: '#B25504',
+      invited: '#3E4784',
+      // ripple
+      'ripple-1': '#EEF4FF',
+      'ripple-2': '#E0EAFF'
+    }
+  },
+  plugins: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+}
+
